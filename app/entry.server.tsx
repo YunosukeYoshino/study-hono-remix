a3e4d12f4b62d2c0b5c7e5783395951df89abaa5
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2024-05-03 11:55:18 +0900

    不要なパッケージの削除

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -6,17 +6,14 @@
 
 import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
+import "dotenv/config";
 import { isbot } from "isbot";
 import { renderToReadableStream } from "react-dom/server";
-import "dotenv/config";
 export default async function handleRequest(
 	request: Request,
 	responseStatusCode: number,
 	responseHeaders: Headers,
 	remixContext: EntryContext,
-	// This is ignored so we can keep it in the template for visibility.  Feel
-	// free to delete this parameter in your app if you're not using it!
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	loadContext: AppLoadContext,
 ) {
 	const body = await renderToReadableStream(
