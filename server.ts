c6073bd8e332d4b048c45470ba17c591300d2268
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2024-05-08 02:44:57 +0900

    update: server.tsに新しいエンドポイントを追加

--- server.ts
+++ server.ts
@@ -1,18 +1,18 @@
 /*  deploy: bunx wrangler deploy server.ts */
 /* dev: bunx wrangler dev server.ts */
 
+import { createRequestHandler } from "@remix-run/cloudflare";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
+import { todos } from "drizzle/schema";
 import { Hono } from "hono";
 // import { basicAuth } from "hono/basic-auth";
 import { secureHeaders } from "hono/secure-headers";
-import { createRequestHandler } from "@remix-run/cloudflare";
 import * as build from "./build/server";
-import { drizzle } from "drizzle-orm/d1";
-import { todos } from "drizzle/schema";
-import { eq } from "drizzle-orm";
 
 const remixHandler = createRequestHandler(build, process.env.NODE_ENV);
 type Bindings = {
-  DB: D1Database;
+	DB: D1Database;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -29,56 +29,71 @@ const app = new Hono<{ Bindings: Bindings }>();
  * todos
  */
 app.get("/todos", async (c) => {
-  const db = drizzle(c.env.DB);
-  const result = await db.select().from(todos).all();
-  return c.json(result);
+	const db = drizzle(c.env.DB);
+	const result = await db.select().from(todos).all();
+	return c.json(result);
+});
+
+/**
+ * todos:id
+ */
+app.get("/todos/:id", async (c) => {
+	const id = Number.parseInt(c.req.param("id"));
+
+	if (Number.isNaN(id)) {
+		return c.json({ error: "invalid ID" }, 400);
+	}
+
+	const db = drizzle(c.env.DB);
+	const result = await db.select().from(todos).where(eq(todos.id, id)).get();
+	return c.json(result);
 });
 
 /**
  * create todo
  */
 app.post("/todos", async (c) => {
-  const params = await c.req.json<typeof todos.$inferSelect>();
-  const db = drizzle(c.env.DB);
-  const result = await db
-    .insert(todos)
-    .values({ title: params.title })
-    .execute();
-  return c.json(result);
+	const params = await c.req.json<typeof todos.$inferSelect>();
+	const db = drizzle(c.env.DB);
+	const result = await db
+		.insert(todos)
+		.values({ title: params.title })
+		.execute();
+	return c.json(result);
 });
 
 /**
  * update todo
  */
 app.put("/todos/:id", async (c) => {
-  const id = Number.parseInt(c.req.param("id"));
+	const id = Number.parseInt(c.req.param("id"));
 
-  if (Number.isNaN(id)) {
-    return c.json({ error: "invalid ID" }, 400);
-  }
+	if (Number.isNaN(id)) {
+		return c.json({ error: "invalid ID" }, 400);
+	}
 
-  const params = await c.req.json<typeof todos.$inferSelect>();
-  const db = drizzle(c.env.DB);
-  const result = await db
-    .update(todos)
-    .set({ title: params.title, isCompleted: params.isCompleted })
-    .where(eq(todos.id, id));
-  return c.json(result);
+	const params = await c.req.json<typeof todos.$inferSelect>();
+	const db = drizzle(c.env.DB);
+	const result = await db
+		.update(todos)
+		.set({ title: params.title, isCompleted: params.isCompleted })
+		.where(eq(todos.id, id));
+	return c.json(result);
 });
 
 /**
  * delete todo
  */
 app.delete("/todos/:id", async (c) => {
-  const id = Number.parseInt(c.req.param("id"));
+	const id = Number.parseInt(c.req.param("id"));
 
-  if (Number.isNaN(id)) {
-    return c.json({ error: "invalid ID" }, 400);
-  }
+	if (Number.isNaN(id)) {
+		return c.json({ error: "invalid ID" }, 400);
+	}
 
-  const db = drizzle(c.env.DB);
-  const result = await db.delete(todos).where(eq(todos.id, id));
-  return c.json(result);
+	const db = drizzle(c.env.DB);
+	const result = await db.delete(todos).where(eq(todos.id, id));
+	return c.json(result);
 });
 
 app.use("*", secureHeaders());
